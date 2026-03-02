6b858e8c20ebd5ea96b6f65a8d0414fbb326501f
Antonio Pino <antoniopino111ia@gmail.com>
2026-03-02 20:49:36 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/antoniopino/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.antoniopino
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Antonio Pino.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
